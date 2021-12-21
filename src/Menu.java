10274c044fdb1d73eb82b65998d35a35a8310187
Bilalnizar1 <96098184+Bilalnizar1@users.noreply.github.com>
2021-12-21 16:44:02 +0530

    j

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,11 @@
 import java.io.InputStream;
 import java.sql.PreparedStatement;
 import java.sql.*;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 class Customer {
     private String name;
     private int id;
@@ -27,144 +31,89 @@ class Customer {
     }
 }
 public class Menu {
-    private static int CustomerID;
-    private static String CustomerName;
+    private static final Logger loggger=Logger.getLogger((Menu.class.getName()));
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException{
             Class.forName("org.sqlite.JDBC");
             String jdbcUrl="jdbc:sqlite:Customer.db";
 
-            try {
-                Connection connection= DriverManager.getConnection(jdbcUrl);
-                if(connection!=null){
-                    DatabaseMetaData meta=connection.getMetaData();
-                    System.out.println("Tha driver name is" +meta.getDriverName());
+            try(Connection connection= DriverManager.getConnection(jdbcUrl)) {
+                if (connection != null) {
+                    DatabaseMetaData meta = connection.getMetaData();
+                    System.out.println("The driver name is {0}" + meta.getDriverName());
                     System.out.println("Database Has been Created");
-                }}
-            catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        Class.forName("org.sqlite.JDBC");
-        Connection conn = DriverManager.getConnection(jdbcUrl);
-            try {
-                System.out.println("Database opened sucessfully");
-                Statement stmt = conn.createStatement();
-                String sql=" CREATE TABLE CUSTOMERSDATA " +
-                            " (CustomerID INT(20) PRIMARY KEY ," +
-                        " CustomerName TEXT(30)) ";
-                System.out.println("Table Created");
-
-                stmt.executeUpdate(sql);
-
-
-            }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            try {
-                String sql = " INSERT INTO CUSTOMERSDATA" + " (CustomerID, " + " CustomerName)" +
-                        "VALUES (?,?)";
-                PreparedStatement stmt=conn.prepareStatement(sql);
-                stmt.setInt(1,CustomerID);
-                stmt.setString(2,CustomerName);
-                stmt.executeUpdate(sql);
-
-            }
-            catch (Exception e){
-                System.out.println(e.getMessage());
-
-            }
-            try{
-                Statement stmt=conn.createStatement();
-                Class.forName("org.sqlite.JDBC");
-                String sql="SELECT* FROM CUSTOMERSDATA";
-                ResultSet rs=stmt.executeQuery(sql);
-                while (rs.next()) {
-                    System.out.println(rs.getInt(CustomerID) + "\t" + rs.getString(CustomerName));
                 }
-            }
-            catch (Exception e){
+           /* catch (Exception e){
                 System.out.println(e.getMessage());
-            }
-            try {
-                Statement stmt=conn.createStatement();
+            }*/
+
+                try (Statement stmt = connection.createStatement()) {
+                    System.out.println("Database opened sucessfully");
+
+                    String sql = " CREATE TABLE IF NOT EXISTS CUSTOMERSDATA " +
+                            " (CustomerID INT PRIMARY KEY ," +
+                            " CustomerName TEXT(30)) ";
+                    System.out.println("Table Created");
+
+                    stmt.executeUpdate(sql);
+
 
                 }
-             catch (SQLException e){
-                System.out.println("Error Connecting Database");
-                e.printStackTrace();
-            }
+                Scanner sc = new Scanner(System.in);
+                int option = 0;
+                while (option != 4) {
+                    displayMenu();
+                    option = sc.nextInt();
+                    if (option == 1) {
+                        System.out.print("Enter the Customer's id: ");
 
+                        int CustomerID = sc.nextInt();
+                        sc.nextLine();
 
+                        System.out.print("Enter the Customer's name: ");
+                        String CustomerName = sc.nextLine();
 
-
-
-
-
-
-
-
-            Customer app=new Customer();
-            Scanner sc = new Scanner(System.in);
-            Customer[] customer = {};
-            int option = 0;
-            while(option != 6) {
-                displayMenu();
-                option = sc.nextInt();
-                if(option == 1) {
-                    System.out.print("Enter the Customer's id: ");
-
-                    int CustomerID = sc.nextInt();
-                    sc.nextLine();
-
-                    System.out.print("Enter the Customer's name: ");
-                    String CustomerName = sc.nextLine();
-                    Customer newCustomer = new Customer(CustomerName, CustomerID);
-
-                    Customer[] temp = new Customer[customer.length + 1];
-                    for(int i=0; i<temp.length-1; i++) {
-                        temp[i] = customer[i];
-                    }
-                    temp[temp.length-1] = newCustomer;
-                    customer = temp.clone();
-                }else if(option == 2) {
-                    for(Customer emp: customer) {
-                        System.out.println(emp.toString());
-                    }
-                }else if(option == 3) {
-                    System.out.print("Enter the Customer's id: ");
-                    int id = sc.nextInt();
-                    int i = 0;
-                    boolean exists = false;
-                    for(i=0; i<customer.length; i++) {
-                        if(customer[i].getID() == id) {
-                            exists = true;
-                            break;
+                        String sql = " INSERT INTO CUSTOMERSDATA" + " (CustomerID, " + " CustomerName)" +
+                                "VALUES (?,?)";
+                        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                            stmt.setInt(1, CustomerID);
+                            stmt.setString(2, CustomerName);
+                            boolean executed = stmt.execute();
+                                loggger.log(Level.INFO, (executed ? "Executed Sucessfully" : "failed to execute"));
                         }
+                    } else if (option == 2) {
+                        try (Statement stmt = connection.createStatement()) {
+                            Class.forName("org.sqlite.JDBC");
+                            String sql = "SELECT * FROM CUSTOMERSDATA";
+                            ResultSet rs = stmt.executeQuery(sql);
+                            while (rs.next()) {
+                                //System.out.println(rs.getInt(CustomerID) + "\t" + rs.getString(CustomerName));
+                                System.out.println(rs.getInt(1) + "-" + rs.getString(2));
+                            }
+
+                        }
+                    } else if (option == 3) {
+                        System.out.println("Enter the Customer id: ");
+                        int id = sc.nextInt();
+
                     }
-                    if(exists) {
-                        System.out.println("Customer's Data: " + customer[i].toString());
-                    }else {
-                        System.out.println("Customer not found.");
-                    }
-                }
-                else if(option == 4) {
                 }
             }
-        }
+            catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
 
 
-        public static void displayMenu() {
-            System.out.println("***-----Menu-----***:" +
-                    "\n [1] Add new Customer"
-                    + "\n [2] Display All Customers"
-                    + "\n [3] Retrieve a Customer's Data"
-                    + "\n [4] Exit"
-                    + "\n \n Enter your choice: ");
-        }
+                    public static void displayMenu () {
+                        System.out.println("***-----Menu-----***:" +
+                                "\n [1] Add new Customer"
+                                + "\n [2] Display All Customers"
+                                + "\n [3] Retrieve a Customer's Data"
+                                + "\n [4] Exit"
+                                + "\n \n Enter your choice: ");
+                    }
 
 
-
-
-    }
+                }
 
